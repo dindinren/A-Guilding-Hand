a1f7ab233d6ad6cefc455aaa4fedabe514aca13b
amee <corvidernity@gmail.com>
2025-01-22 16:51:27 +0800

    play
    +play button

--- a guilding hand/Assets/Scripts/MainMenu.cs
+++ a guilding hand/Assets/Scripts/MainMenu.cs
@@ -4,18 +4,6 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-   /* void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-   */
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
