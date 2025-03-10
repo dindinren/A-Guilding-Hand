c27b19db2d71aa78d986baec9ba985481418fdd3
dindinren <joycelynb21@gmail.com>
2025-03-10 13:50:49 +0800

    Added Animations to the Pause Menu
    +Now it has fancy "Animation"
    
    TODO:
    //Polishing

--- a guilding hand/Assets/PauseMenu.cs
+++ a guilding hand/Assets/PauseMenu.cs
@@ -1,23 +1,32 @@
+using System.Collections;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
     public GameObject pauseMenu;
     public bool isPause = false;
+
+    public Animator anim;
+
     public void Pause()
     {
         pauseMenu.SetActive(true);
         isPause = true;
         Time.timeScale = 0f;
+
+        anim.Play("PauseMenu_OnEnter");
     }
 
     public void Resume()
     {
+        anim.Play("PauseMenu_OnExit");
+        Debug.Log("DOES THE ANIMATION PLAYS");
+
         pauseMenu.SetActive(false);
         isPause = false;
         Time.timeScale = 1f;
     }
-
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
